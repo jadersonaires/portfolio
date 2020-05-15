a336aa8f4fea1d722da7a1c086878e28a7339568
Jaderson Aires <jadersonaires@gmail.com>
2020-05-14 22:21:00 -0300

    favicon e media query

--- src/help/formateDate.js
+++ src/help/formateDate.js
@@ -2,6 +2,6 @@ import moment from 'moment'
 import 'moment/locale/pt-br'
 
 export const formateDateMonthYear = (date, format, separador) => {
-    const dateMonthYear = moment(date).locale('pt-br').format("MMMM [de] YYYY")
+    const dateMonthYear = moment(date).locale('pt-br').format("MMM [de] YYYY")
     return dateMonthYear
 }
